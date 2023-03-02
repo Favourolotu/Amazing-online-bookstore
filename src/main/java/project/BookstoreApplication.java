8c20733085f94a10511b57a83354aa4ebf2f8273
FavourO2 <72259260+Favourolotu@users.noreply.github.com>
2023-03-02 11:04:02 -0500

    Created the structure for the project i.e. the packages and the main file.

---
+++ src/main/java/project/BookstoreApplication.java
@@ -0,0 +1,12 @@
+package project;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+}
